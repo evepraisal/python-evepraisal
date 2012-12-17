ec83b3fb42531274c11a2906a56db04701ed30e5
Kevin McDonald <k3vinmcdonald@gmail.com>
2012-12-16 23:36:22 -0600

    Improves How Datatypes are Populated
    Data types are now populated from Eve game cache. This might not be a
    good long-term solution but it works extremely well now.

--- populate_types.py
+++ populate_types.py
@@ -2,34 +2,26 @@
 # This is a script intended to be ran only when there are updates to the item
 # database. The results are dumped into a file as JSON to be read by the app.
 #
-# Public mySQL access to this data can be found here:
-# http://wiki.eve-id.net/CCP_Database_Dump_Resources
+# This script requires an installed (and updated) copy of Eve Online. This
+# requires Reverence, a tool which looks at the game cache to get up-to-date
+# data. URL: https://github.com/ntt/reverence/
 
 import json
-import mysql.connector
+from reverence import blue
 
 
 if __name__ == '__main__':
-    config = {
-      'user': 'evedump',
-      'password': 'evedump1234',
-      'host': 'db.descention.net',
-      'database': 'evedump',
-      'raise_on_warnings': True,
-    }
-
-    cnx = mysql.connector.connect(**config)
-    cursor = cnx.cursor()
-    query = ("SELECT typeID, groupID, typeName FROM invTypes;")
-    cursor.execute(query)
-
+    eve_path = '/Applications/EVE Online.app/Contents/Resources/EVE Online.app/Contents/Resources/transgaming/c_drive/Program Files/CCP/EVE'
+    # eve_path = 'C:\\Program Files\\CCP\\EVE\\'
+    # eve_path = 'C:\\Program Files (x86)\\CCP\\EVE\\'
+    eve = blue.EVE()
+    cfg = eve.getconfigmgr()
     all_types = {}
-    for (typeID, groupID, typeName) in cursor:
+    for (typeID, groupID, typeName) in cfg.invtypes.Select('typeID',
+                                                    'groupID', 'typeName'):
         all_types[typeName.strip().lower()] = {'typeID': typeID,
                                                'groupID': groupID,
                                                'typeName': typeName}
 
     with open('data/types.json', 'w') as f:
         f.write(json.dumps(all_types, indent=2))
-
-    cnx.close()
