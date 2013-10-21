24521af5369cd8534a7061c1268eeffb23b58208
Kevin McDonald <k3vinmcdonald@gmail.com>
2013-10-20 19:00:04 -0500

    Removes humanize as a requirement. Resolves #6

--- evepraisal/filters.py
+++ evepraisal/filters.py
@@ -1,7 +1,21 @@
-import datetime
-import humanize
+import time
+import math
+from datetime import timedelta
+from babel.dates import format_timedelta
 
 from . import app
+
+
+# Adopted from http://stackoverflow.com/a/3155023/74375
+def millify(n, format="{:,.2f}"):
+    millnames = ['', 'Thousand', 'Million', 'Billion', 'Trillion',
+                 'Quadrillion', 'Quintillion', 'Sextillion', 'Septillion',
+                 'Octillion', 'Nonillion', 'Decillion']
+    millidx = max(0, min(len(millnames) - 1,
+                         int(math.floor(math.log10(abs(n)) / 3.0))))
+    num = n / 10 ** (3 * millidx)
+    return (format + ' {}').format(num,
+                                   millnames[millidx])
 
 
 @app.template_filter('format_isk')
@@ -16,10 +30,10 @@ def format_isk(value):
 def format_isk_human(value):
     if value is None:
         return ""
-    try:
-        return "%s ISK" % humanize.intword(value, format='%.2f')
-    except:
-        return str(value)
+    if value == 0:
+        return "0 ISK"
+
+    return "%s ISK" % millify(value)
 
 
 @app.template_filter('format_volume')
@@ -31,17 +45,25 @@ def format_volume(value):
             return "%.4fm<sup>3</sup>" % value
         if value < 1:
             return "%.2fm<sup>3</sup>" % value
-        return "%sm<sup>3</sup>" % humanize.intcomma(int(value))
+        return "{:,.2f}m<sup>3</sup>".format(value)
     except:
         return "unknown m<sup>3</sup>"
 
 
 @app.template_filter('relative_time')
 def relative_time(past):
+    now = time.time()
+    delta_seconds = now-past
+    if abs(delta_seconds) < 1:
+        return 'just now'
+    postfix = ' ago'
+    if past > now:
+        postfix = ' in the future'
     try:
-        return humanize.naturaltime(datetime.datetime.fromtimestamp(past))
+        delta = timedelta(seconds=delta_seconds)
+        return format_timedelta(delta, locale='en_US') + postfix
     except:
-        return ""
+        return ''
 
 
 @app.template_filter('bpc_count')
