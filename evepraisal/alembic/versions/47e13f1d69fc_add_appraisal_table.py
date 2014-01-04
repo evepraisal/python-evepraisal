e1a3f1ef91676c64b391dd1662d6d804f72f467b
Kevin McDonald <k3vinmcdonald@gmail.com>
2014-01-04 16:24:40 -0600

    Reworked the migration to use far less memory

--- evepraisal/alembic/versions/47e13f1d69fc_add_appraisal_table.py
+++ evepraisal/alembic/versions/47e13f1d69fc_add_appraisal_table.py
@@ -15,6 +15,8 @@ import json
 # import sqlalchemy as sa
 import evepaste
 
+from sqlalchemy.exc import IntegrityError
+
 from evepraisal.models import Appraisals
 from evepraisal import db
 from evepraisal.parser import parse
@@ -42,46 +44,62 @@ def upgrade():
     FAILED_COUNT = 0
     MODIFIED_COUNT = 0
     SUCCESS_COUNT = 0
-    COMMIT_RATE = 1000
-    for i, scan in enumerate(Scans.query.filter()):
-        scan_data = json.loads(scan.Data)
-        prices = [[item.get('typeID'), {'all': item.get('all'),
-                                        'buy': item.get('buy'),
-                                        'sell': item.get('sell')}]
-                  for item in scan_data['line_items']]
+    DUPLICATE_COUNT = 0
 
-        try:
-            kind, result, bad_lines = parse(scan_data['raw_paste'])
-        except evepaste.Unparsable:
-            print('--[Unparsable]---------')
-            print()
-            print([scan_data['raw_paste']])
-            print('-'*20)
-            kind = 'listing'
-            result = [{'name': item['typeName'], 'quantity': item['count']}
+    offset = 0
+    result_count = 1000
+    results = Scans.query.limit(result_count).offset(offset)
+    scans = list(results)
+    while scans:
+        print("Migrating batch of %s. offset=%s" % (result_count, offset))
+        for i, scan in enumerate(scans):
+            scan_data = json.loads(scan.Data)
+            prices = [[item.get('typeID'), {'all': item.get('all'),
+                                            'buy': item.get('buy'),
+                                            'sell': item.get('sell')}]
                       for item in scan_data['line_items']]
-            bad_lines = scan_data['bad_line_items']
-            FAILED_COUNT += 1
 
-        appraisal = Appraisals(Id=scan.Id,
-                               Created=scan.Created,
-                               RawInput=scan_data['raw_paste'],
-                               Parsed=result,
-                               Kind=kind,
-                               BadLines=bad_lines,
-                               Prices=prices,
-                               Market=int(scan_data['solar_system']),
-                               Public=scan.Public,
-                               UserId=scan.UserId)
-        db.session.add(appraisal)
-        SUCCESS_COUNT += 1
-        if i % COMMIT_RATE == 0:
-            db.session.commit()
+            try:
+                kind, result, bad_lines = parse(scan_data['raw_paste'])
+            except evepaste.Unparsable:
+                print('--[Unparsable]---------')
+                print()
+                print([scan_data['raw_paste']])
+                print('-'*20)
+                kind = 'listing'
+                result = [{'name': item['typeName'],
+                           'quantity': item['count']}
+                          for item in scan_data['line_items']]
+                bad_lines = scan_data['bad_line_items']
+                FAILED_COUNT += 1
 
-    db.session.commit()
-    print "Sucesses", SUCCESS_COUNT
-    print "Modified", MODIFIED_COUNT
-    print "Failed", FAILED_COUNT
+            appraisal = Appraisals(Id=scan.Id,
+                                   Created=scan.Created,
+                                   RawInput=scan_data['raw_paste'],
+                                   Parsed=result,
+                                   Kind=kind,
+                                   BadLines=bad_lines,
+                                   Prices=prices,
+                                   Market=int(scan_data['solar_system']),
+                                   Public=scan.Public,
+                                   UserId=scan.UserId)
+
+            # print("Inserting appraisal #%s" % appraisal.Id)
+            db.session.add(appraisal)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                DUPLICATE_COUNT += 1
+            else:
+                SUCCESS_COUNT += 1
+        offset += result_count
+        results = Scans.query.limit(result_count).offset(offset)
+        scans = list(results)
+
+    print("Sucesses: %s" % SUCCESS_COUNT)
+    print("Modified: %s" % MODIFIED_COUNT)
+    print("Failed: %s" % FAILED_COUNT)
+    print("Duplicates: %s" % DUPLICATE_COUNT)
 
 
 def downgrade():
